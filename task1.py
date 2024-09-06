8e04144fc1decbb0bc56837d2dc9a5b553f9c2e4
vikam1992 <vikam1992@yandex.ru>
2024-09-06 16:07:24 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+# '1st program'
+print (9 ** 0.5 *5)
+# '2nd program'
+print (9.99 > 9.98 and 1000 != 1000.1)
+# '3rd program'
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+print (2 * 2 + 2 == 2 * (2 + 2))
+# '4th program'
+print(float('123.456'))
+print (123.456 * 10)
+print (int(1234.56 % 10))
