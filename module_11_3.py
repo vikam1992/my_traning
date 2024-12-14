ebf95e3575407c8842182e8559ce119ca3cc7702
vikam1992 <vikam1992@yandex.ru>
2024-12-14 12:47:18 +0300

    Update module_11_3.py

--- module_11_3.py
+++ module_11_3.py
@@ -4,9 +4,16 @@ from inspect import getmodule
 
 def introspection_info(obj):
     dict_introspection = dict()
+    attributes = []
+    methods = []
+    for attr in dir(obj):
+        if callable(getattr(obj, attr)):
+            methods.append(attr)
+        else:
+            attributes.append(attr)
     dict_introspection['type'] = type(obj) # тип объекта
-    dict_introspection['attribute'] = dir(obj) # атрибуты объекта
-    dict_introspection['method'] = hasattr(obj, 'upper') # ищем методы объекта
+    dict_introspection['attribute'] = attributes # атрибуты объекта
+    dict_introspection['method'] = methods # ищем методы объекта
     dict_introspection['module_obj'] = getmodule(obj) # проверяем к какому модулю относится
     dict_introspection['module_func'] = getmodule(introspection_info) # функция относится к модулю __main__
     return dict_introspection
